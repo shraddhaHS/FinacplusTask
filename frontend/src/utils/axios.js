886c984e687764e0218070d34a1aa3d2dacf9b07
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2025-03-09 15:15:34 +0530

    commit

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:5001/api" 
+//       : import.meta.env.BASE_URL, 
+//   withCredentials: true,
+// });
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api" 
-      : import.meta.env.BASE_URL, 
-  withCredentials: true,
-});
+  baseURL: import.meta.env.MODE==="development"? "http://localhost:5001/api": "/api",
+  withCredentials:true
+})
