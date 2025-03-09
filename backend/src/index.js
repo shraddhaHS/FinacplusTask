943784b008e1f159099c8d0a4702b889d7496dcb
shraddha1dir@gmail.com <shraddha1dir@gmail.com>
2025-03-09 14:38:15 +0530

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
-import { connectDB } from "./src/config/db.js"
+import { connectDB } from "./config/db.js"
 import cors from "cors"
-import userRoutes from "./src/routes/user.route.js"
+import userRoutes from "./routes/user.route.js"
 import cookieParser from "cookie-parser";
+import path from "path"
 
 dotenv.config()
+const __dirname  = path.resolve()
 
 const app = express()
 
@@ -29,6 +31,14 @@ app.use(cors({
 ))
 
 app.use( "/api/user" ,userRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.get("/" , (req,res) => {
     res.send("app is running")
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api" 
-      : import.meta.env.VITE_BASE_URL, 
+      : import.meta.env.BASE_URL, 
   withCredentials: true,
 });
